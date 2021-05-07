2e07980cb505f702c33877b389d39f93a2619e07
Raul Duarte <raul@grafftecnologia.com.br>
2021-05-06 21:56:01 -0300

    refactor: update navbar and spinner components

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -7,23 +7,23 @@
     <v-navigation-drawer v-model="drawer" absolute temporary>
       <v-list nav dense>
         <v-list-item-group v-model="group" active-class="blue-grey darken-4--text text--accent-4">
-          <v-list-item>
+          <v-list-item to="/">
             <v-list-item-icon>
               <v-icon>mdi-home</v-icon>
             </v-list-item-icon>
             <v-list-item-title>Home</v-list-item-title>
           </v-list-item>
-          <v-list-item>
+          <v-list-item to="/tasks">
             <v-list-item-icon>
               <v-icon>mdi-card-text</v-icon>
             </v-list-item-icon>
             <v-list-item-title>Tasks</v-list-item-title>
           </v-list-item>
-          <v-list-item>
+          <v-list-item to="/signin">
             <v-list-item-icon>
               <v-icon>mdi-account-plus</v-icon>
             </v-list-item-icon>
-            <v-list-item-title>Sign Up</v-list-item-title>
+            <v-list-item-title>Sign In</v-list-item-title>
           </v-list-item>
         </v-list-item-group>
       </v-list>
---
+++ src/components/loading/Spinner.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="text-center">
+    <v-progress-circular :size="200" color="black" indeterminate></v-progress-circular>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Spinner'
+}
+</script>

291ac5b65e6dfeac8d818f60806d050a77f0cbec
Raul Duarte <raul@grafftecnologia.com.br>
2021-05-09 01:11:59 -0300

    feat: home page

---
+++ src/views/base/pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    <h1>Home Page</h1>
+    <v-card> Hello, ma friend </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
